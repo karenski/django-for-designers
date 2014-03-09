b8c58d23a44b81a12d00ec464a4e8c6ebe97df28
karenski <rinalias@alum.dartmouth.org>
2014-03-09 11:48:41 -0700

    added urlconf

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'bookmarks.views.index', name ='home'),
+    url(r'^bookmarks/$', 'bookmarks.views.index', name = 'bookmarks_view'),
+    url(r'^tags/([\w-]+)/$'), 'bookmarks.views.tag'),
     url(r'^admin/', include(admin.site.urls)),
 )
