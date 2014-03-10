aca078982bd827f67a05861612ba32ff461aa573
karenski <rinalias@alum.dartmouth.org>
2014-03-10 16:03:51 -0700

    end of part 3

--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from bookmarks.models import Bookmark, Tag
 
 # Create your views here.
 
 def index(request):
-	context = {}
+	bookmarks = Bookmark.objects.all().order_by('-timestamp')[:10]
+	context = {
+		'bookmarks': bookmarks
+	}
 	return render(request, 'index.html', context)
 
 def tag(request, tag_name):
+	tag = get_object_or_404(Tag, slug=tag_name)
+	bookmarks = tag.bookmarks.all()
 	context = {
-		'tag': tag_name
+		'tag': tag,
+		'bookmarks': bookmarks,
 	}
 	return render(request, 'tag.html', context)
 
