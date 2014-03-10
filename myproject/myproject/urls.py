b48192811f22fe60442d0a43cd494bd32f6cfdb9
karenski <rinalias@alum.dartmouth.org>
2014-03-10 14:58:39 -0700

    added logout and login

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 	url(r'^$', 'bookmarks.views.index', name='home'),
     url(r'^bookmarks/$', 'bookmarks.views.index', name='bookmarks_view'),
-    url(r'^tags/([\w-]+)/$', 'bookmarks.views.tag'),
+    url(r'^tags/(\w+)/$', 'bookmarks.views.tag'),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^login/', 'django.contrib.auth.views.login'),
+	url(r'^logout/','django.contrib.auth.views.logout', {'next_page': '/"'}),
 )
+
