ee73364d2ef056bcddc3b3f429c08c7b343f2c72
karenski <rinalias@alum.dartmouth.org>
2014-03-10 14:37:47 -0700

    following update to Bookmark to add author field

--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	context = {}
+	return render(request, 'index.html', context)
+
+def tag(request, tag_name):
+	context = {
+		'tag': tag_name
+	}
+	return render(request, 'tag.html', context)
+
