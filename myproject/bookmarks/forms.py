b5422dcd37cee6529832e4c48ccfb4e42a97d2ec
karenski <rinalias@alum.dartmouth.org>
2014-03-11 16:43:16 -0700

    created form for adding bookmarks

---
+++ myproject/bookmarks/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from bookmarks.models import Bookmark
+
+class BookmarkForm(forms.ModelForm):
+	class Meta:
+		model = Bookmark
+		fields = ('author', 'url', 'title')
+		widgets = {
+			'author': forms.HiddenInput()
+		}
+	tags = forms.CharField(max_length=100, required=False)
--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,12 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from bookmarks.models import Bookmark, Tag
-
+from bookmarks.forms import BookmarkForm
+import urllib
  
 def index(request):
+    if request.method == 'POST':
+    	form = BookmarkForm(request.POST)
+    	if form.is_valid():
+    		new_bookmark = form.save()
+    		raw_tags = form.cleaned_data['tags'].split(',')
+    		if raw_tags:
+    			for raw_tag in raw_tags:
+    				raw_tag = raw_tag.strip().replace(' ','_').lower()
+    				tag_slug = urllib.quote(raw_tag)
+    				tag, created = Tag.objects.get_or_create(slug=tag_slug)
+    				tag.save()
+    				tag.bookmarks.add(new_bookmark)
+
+    		return redirect(index)
     bookmarks = Bookmark.objects.all().order_by('-timestamp')[:10]
+    current_user = request.user
+    form = BookmarkForm(initial={'author':current_user})
     context = {
         'bookmarks': bookmarks,
+        'form': form,
     }
     return render(request, 'index.html', context)
 
